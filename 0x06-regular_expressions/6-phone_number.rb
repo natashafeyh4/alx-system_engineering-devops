969502dc875cc5aaf07a9a5b74cd847761d6ec80
natashafeyh4 <natashafeyh44@gmail.com>
2023-11-29 10:33:26 -0800

    update

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(input)
-    regex =  /\A\d{10}\z/
-
-    match_result = input.match(regex)
-
-    puts match_result ? match_result[0] : ''
-end
+puts ARGV[0].scan(/^\d{10}$/).join
