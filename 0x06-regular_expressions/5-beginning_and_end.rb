50a62fc58fd74fdf89605c529675b895119cc5ac
natashafeyh4 <natashafeyh44@gmail.com>
2023-11-29 10:25:41 -0800

    update

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(input)
-    regex = /^hn$/
-
-    match_result = input.match(regex)
-
-    puts match_result ? match_result[0] : ''
-end
+puts ARGV[0].scan(/^h.n$/).join
